549873d046d592b46f1f146fd44d012731991f44
Matteo <gale93@gmail.com>
2018-03-28 12:41:13 +0200

    cleaned player controller code, start implementing it

--- src/systems/playercontrolsystem.cpp
+++ src/systems/playercontrolsystem.cpp
@@ -1,14 +1,11 @@
 #include "playercontrolsystem.hpp"
 
-
 #include "components\playercontroller.hpp"
 #include "components\body.hpp"
-
 
 void PlayerControlSystem::update(const float dt)
 {
 	registry->view<PlayerController, Body>().each([](auto entity, PlayerController &c, Body &bodycomponent) {
-
 	});
 }
 
